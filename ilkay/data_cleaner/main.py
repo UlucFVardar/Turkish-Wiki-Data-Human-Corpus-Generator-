88942f599dc889c36f5ff4f6a397a21698c48db6
Ilkay Tevfik Devran <devrani@mef.edu.tr>
2018-11-10 03:35:25 +0300

    The requested format of the output is almost done.

--- ilkay/data_cleaner/main.py
+++ ilkay/data_cleaner/main.py
@@ -13,12 +13,18 @@ def main():
                     dc.process_bulk_paragraph(line.split("#")[4].strip())
                     #print line.split("#")[0].strip(), '\n', dc.clean_sentence
                     o_f.write( line.split("#")[0].strip() + '\n' + dc.clean_sentence + '\n\n')
-                else:
-                    pass
-                    #print '(:'
         o_f.close()
     f.close()
-
+    
+    """
+    with open("../data/O4_2018-10-06_articles-allwere alooking.txt", "r") as f:
+        with open("../data/compressed_original_data.txt","w") as o_f:
+            for line in f:
+                if not line.startswith('\n') and line.endswith('\n'):
+                    o_f.write(line)
+        o_f.close()
+    f.close()
+    """
  
 if __name__ == '__main__':
     main()
--- ilkay/output_fixer/sentence_combiner.py
+++ ilkay/output_fixer/sentence_combiner.py
@@ -1,31 +1,36 @@
 # -*- coding: UTF-8 -*-
+import itertools
 
-def main():
-    
-    with open("../outputs/splitted_outputs_v2.txt", "r") as f:
-        with open("../outputs/combined_sentences.txt","w") as o_f:
-            for line in f:
-                line_parts = line.strip().split('#')
-                first_sentence = line_parts[1]
-                for i, part in enumerate(line_parts):
-                    try:
-                        if part.endswith('(d.'):
-                            first_sentence += line_parts[i+1]
-                        if part.endswith('o.'):
-                            first_sentence += line_parts[i+1]
-                    except:
-                        if '(d.' not in part:
-                            first_sentence = line_parts[1]
-                        else:
-                            first_sentence = 'Faulty'
-                    
-                o_f.write( line_parts[0] + '#' + first_sentence + '\n')
-        o_f.close()
+def main(splitted_output, data_wo_end_of_line, output):
+    with open(splitted_output, "r") as f:
+        with open(data_wo_end_of_line, "r") as original_f:
+            with open(output,"w") as o_f:
+                #for line in f:
+                for line, line2 in itertools.izip(f, original_f):
+                    line_parts = line.strip().split('#')
+                    first_sentence = line_parts[1]
+                    for j, part in enumerate(line_parts):
+                        try:
+                            if part.endswith('(d.'):
+                                first_sentence += line_parts[j+1]
+                            if part.endswith('o.'):
+                                first_sentence += line_parts[j+1]
+                        except:
+                            if '(d.' not in part:
+                                first_sentence = line_parts[1]
+                            else:
+                                first_sentence = 'None-Faulty'
+                        
+                    o_f.write( line_parts[0] + '#' + line2.split("#")[3] + "#" + first_sentence + '\n')
+            o_f.close()
+        original_f.close()
     f.close()
-
     print 'Done canim! :)'
 
 
 
 if __name__ == "__main__":
-    main()
+    main("../outputs/splitted_outputs_v2.txt",
+    "../data/compressed_original_data.txt",
+    "../outputs/combined_sentences.txt"
+    )
