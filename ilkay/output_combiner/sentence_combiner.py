d761b6f9a89c946e8c02174476a7f5fc91d39aa0
Ilkay Tevfik Devran <devrani@mef.edu.tr>
2018-11-10 12:11:09 +0300

    json format added like as we talked about for cpmbined_sentences.txt

--- ilkay/output_combiner/sentence_combiner.py
+++ ilkay/output_combiner/sentence_combiner.py
@@ -1,29 +1,59 @@
 # -*- coding: UTF-8 -*-
 import itertools
+import json
 
 def main(splitted_output, data_wo_end_of_line, output):
-    with open(splitted_output, "r") as f:
+    """
+        @splitted_output: output from zemberek Java Code
+        @data_wo_end_of_line: this is the new format is created from bulk data file
+        @output: is the last version that is required
+    """
+
+    with open(splitted_output, "r") as splitted_f:
         with open(data_wo_end_of_line, "r") as original_f:
-            with open(output,"w") as o_f:
-                for line, line2 in itertools.izip(f, original_f):
+            with open(output,"w") as output_f:
+                
+                for line, line2 in itertools.izip(splitted_f, original_f):
                     line_parts = line.strip().split('#')
                     first_sentence = line_parts[1]
+                    second_sentence = ''
+                    sentences = {}
+                    d_flag = False
+                    o_flag = False
+                    lngth = len(line_parts)
                     for j, part in enumerate(line_parts):
                         try:
-                            if part.endswith('(d.'):
+                            if part.endswith('o.') and o_flag == False:
                                 first_sentence += line_parts[j+1]
-                            if part.endswith('o.'):
-                                first_sentence += line_parts[j+1]
+                                o_flag = True
+                            
+                            if d_flag == True:
+                                if o_flag == True:
+                                    try:
+                                        second_sentence = line_parts[4]
+                                    except:
+                                        second_sentence = 'None'
+                                else:
+                                    try:
+                                        second_sentence = line_parts[3]
+                                    except:
+                                        second_sentence = 'None'
+                            
+                            if part.endswith('(d.') and d_flag == False:
+                                first_sentence = line_parts[j] + line_parts[j+1]
+                                d_flag = True
                         except:
                             if '(d.' not in part:
                                 first_sentence = line_parts[1]
                             else:
                                 first_sentence = 'None-Faulty'
-                        
-                    o_f.write( line_parts[0] + '#' + line2.split("#")[2] + "#" + line2.split("#")[3] + "#" + first_sentence + '\n')
-            o_f.close()
+                    #print line_parts[0], '\n1. ', first_sentence, '\n2. ',second_sentence, '\n'
+                    sentences['s1'] = first_sentence
+                    sentences['s2'] = second_sentence
+                    output_f.write(line_parts[0] + '#' + line2.split("#")[2] + "#" + line2.split("#")[3] + "#" + json.dumps(sentences) + '\n')
+            output_f.close()
         original_f.close()
-    f.close()
+    splitted_f.close()
     print 'Done canim! :)'
 
 
